bbbc46932dcc78bf6adb356b7bb5ee2e55c110f6
freakkid <904811062@qq.com>
2018-03-17 14:51:46 +0800

    begin to run

--- bin/index.js
+++ bin/index.js
@@ -7,10 +7,12 @@ import process from 'process';
 import { port as defaultPort } from '../config';
 import { logger } from '../utils';
 
-var port = normalizePort(process.env.PORT || defaultPort);
+var port = normalizePort(process.env.PORT) || defaultPort;
 
-app.listen(port);
-
+var server = http.createServer(app.callback());
+server.listen(port);
+server.on('error', onError);
+server.on('listening', onListening);
 
 function normalizePort(val) {
   let port = parseInt(val, 10);
@@ -21,4 +23,36 @@ function normalizePort(val) {
     return port;
   }
   return false;
+}
+
+function onError(error) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  let bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      logger.error(bind + ' requires elevated privileges');
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      logger.error(bind + ' is already in use');
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+function onListening() {
+  let addr = server.address();
+  let bind = typeof addr === 'string'
+    ? 'pipe ' + addr
+    : 'port ' + addr.port;
+  logger.info('Listening on ' + bind);
 }
