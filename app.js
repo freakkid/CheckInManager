0df0885be661c80cdc2bcfe75f6b73fd61fb1417
LebronX <782813460@qq.com>
2018-04-30 15:38:20 +0800

    hello

---
+++ app.js
@@ -0,0 +1,38 @@
+import Koa from 'koa';
+const app = new Koa();
+import json from 'koa-json';
+import bodyparser from 'koa-bodyparser';
+import logger from 'koa-logger';
+
+// import router from './routes/index';
+
+app.use(bodyparser({
+  enableTypes: ['json', 'form', 'text']
+}));
+app.use(json());
+app.use(logger());
+
+// handle error
+const handler = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.response.status = 500;
+    ctx.response.body = 'Server error';
+  }
+};
+
+app.use(handler);
+
+// logger
+app.use(function* (next) {
+  const start = new Date();
+  yield next;
+  const ms = new Date() - start;
+  logger.log(`${this.method} ${this.url} - ${ms}ms`);
+});
+//haha
+// // router
+// app.use(router.routes());
+
+export default app;
