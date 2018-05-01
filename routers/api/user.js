c95dc656c050f4b443955bfd06bc0e6474040e7a
freakkid <904811062@qq.com>
2018-05-01 21:35:59 +0800

    控制层api/user部分

--- routers/api/user.js
+++ routers/api/user.js
@@ -1,13 +1,13 @@
 import Router from 'koa-router';
-// import { userCtrl } from '../controllers';
+import { apiUserCtrl } from '../../controllers';
 
 export const router = new Router();
 
 // PATCH  api/user/password
-router.patch('api/user/password');
+router.patch('api/user/password', apiUserCtrl.changePassword);
 
 // POST /api/user
-router.post('/api/user');
+router.post('/api/user', apiUserCtrl.createUser);
 
 // DELETE /api/user/{user_id}
-router.delete('/api/user/{user_id}');
+router.delete('/api/user/{user_id}', apiUserCtrl.deleteUser);
