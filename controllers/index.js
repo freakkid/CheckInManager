1129aa6a044ed9dafdcacfb64ed778907447ab74
freakkid <904811062@qq.com>
2018-05-01 19:54:26 +0800

    完成控制层的checkinByQRCode

--- controllers/index.js
+++ controllers/index.js
@@ -2,5 +2,7 @@ import * as userCtrl from './user';
 import * as courseCtrl from './course';
 import * as studentCtrl from './student';
 import * as addStudentCtrl from './add_student';
+import * as checkinByQRCodeCtrl from './checkinByQRCode';
 
-export { userCtrl, courseCtrl, studentCtrl, addStudentCtrl };
+export { userCtrl, courseCtrl, studentCtrl, addStudentCtrl, 
+  checkinByQRCodeCtrl };
