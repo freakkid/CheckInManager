bb08a209672baff5f25b4b3d8e724d8144e33b98
freakkid <904811062@qq.com>
2018-05-22 19:53:47 +0800

    给 GET /course 增加username字段+修复npm run test时需要启动redis-server的错误

--- services/index.js
+++ services/index.js
@@ -2,7 +2,7 @@ import * as validator from './validator';
 import * as checkinServ from './checkin/checkin';
 import * as sessionServ from './user/session';
 
-export { generateCheckinID } from './checkin/checkin';
+// export { generateCheckinID } from './checkin/checkin';
 export { blockUnsignedVisitors } from './user/blockUnsignedVisitors'
 export { md5Hash } from './user/md5';
 export { validator, checkinServ, sessionServ };
--- test/model_test/checkin_course_test.js
+++ test/model_test/checkin_course_test.js
@@ -1,8 +1,15 @@
 import assert from 'assert';
+import crypto from 'crypto';
+
 import { checkinCourseModel } from '../../models';
-import { generateCheckinID } from '../../services/';
+// import { generateCheckinID } from '../../services/';
 import { courses } from './course_test';
 
+function generateCheckinID(course_id) {
+  return crypto.createHmac('sha1', Date.now().toString())
+    .update(course_id + 'ugnamsung 15331117' + new Date())
+    .digest('hex');
+}
 
 export var checkinIDs = new Array(courses.length).fill().map((_, i) => generateCheckinID(i + 1));
 
